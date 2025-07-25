24e5fabc0cb6b7816598ee9bf631d79761922431
Bolaji <adeoyebolaji30@gmail.com>
2025-07-25 17:52:16 +0100

    Updated app

--- src/WebPage/Music.js
+++ src/WebPage/Music.js
@@ -40,11 +40,42 @@ const Music = () => {
   }
 };
 
+ 
+
+useEffect(() => {
+  const home = async () => {
+  setError('');
+  setLoading(true);
+
+  try {
+    setError('');
+    const res = await axios.get(
+      `https://corsproxy.io/?https://api.deezer.com/search?q=wizkid`
+    );
+
+    setTracks(res.data.data);
+  } catch (err) {
+    setError('Oops! Something went wrong.');
+  }
+
+  finally{
+     setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }
+};
+
+home();
+}, []);
+
   return (
   <div className="main-container">
-  <h2 className="music-header">Yoyoyo Pop Music</h2>
+    <div className='music-title'>
+    <div className='music-header'>
+      <h2>Yoyoyo Pop Music</h2>
+    </div>
+    </div>
   <div className="search-bar">
-    <button onClick={handleSearch}>Search</button>
 
     <input
       type="text"
@@ -53,14 +84,40 @@ const Music = () => {
       onChange={(e) => setQuery(e.target.value)}
     />
 
+    <button onClick={handleSearch}>Search</button>
+
     {loading && <div className="spinner"></div>}
 
 
-    {error && <p className="error-msg">{error}</p>}
+    {/* {error && <p className="error-msg">{error}</p>} */}
 
   </div>
 
-  {/* 🎧 Show music results below the search bar */}
+  <div className='track-list'>
+  {tracks.map((track, index) => (
+    <div key={track.id} className="track-card">
+          <img src={track.album.cover_medium} alt={track.title} />
+          <h4>{track.title}</h4>
+          <p>{track.artist.name}</p>
+
+          <audio
+            controls
+            ref={(el) => (audioRefs.current[index] = el)}
+            src={track.preview}
+            onPlay={() => {
+              audioRefs.current.forEach((audio, idx) => {
+                if (audio && idx !== index) {
+                  audio.pause();
+                  audio.currentTime = 0;
+                }
+              });
+            }}
+          />
+        </div>
+  ))}
+  </div>
+
+  
   <div className="track-list">
     {tracks.length === 0 ? (
       <p className='no-tex-msg'>No tracks yet. Try searching!</p>
